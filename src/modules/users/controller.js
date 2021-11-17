118d2cccb8c31439401b12f0bb5a0ea84d02cc3e
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-17 19:00:55 +0530

    Day -1

---
+++ src/modules/users/controller.js
@@ -0,0 +1,7 @@
+exports.getUser = (req , res) =>{
+    res.send("I am the User");
+}
+
+exports.deleteUser = () =>{
+    
+}
--- src/modules/users/router.js
+++ src/modules/users/router.js
@@ -1,3 +1,6 @@
+const { Router } = require('express')
 const express = require('express')
 const app = express.Router()
 
+const {getUser} = require("./controller");
+Router.get("/" , getUser);
