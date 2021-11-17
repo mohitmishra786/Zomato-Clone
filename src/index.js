f3f7eea5dac2753e5e4dff4814667ca22d0d44d8
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-17 18:55:05 +0530

    Day -1

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const PORT = 2000
 
 // '/' it is the basic route for our req and res if we will use '/hello' then our basic workinf is from hello
 app.get("/",(req,res) =>{
-    console.log({res});
-    res.send('Hello World');
+   // console.log({res});
+    res.status(200).send("Hello World");
+    //res.send('Hello World');
 })
 
 
 app.post('/', function (req, res) {
     
-    res.send('Got a POST request')
+    res.send('Got a POST request');
   })
 
 app.listen(PORT, () => {
