d7ba4d48ea46d6ddce0c538477ab24b5edbb3c9b
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-17 18:51:08 +0530

    First Day

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const PORT = 2000
+
+
+// '/' it is the basic route for our req and res if we will use '/hello' then our basic workinf is from hello
+app.get("/",(req,res) =>{
+    console.log({res});
+    res.send('Hello World');
+})
+
+
+app.post('/', function (req, res) {
+    
+    res.send('Got a POST request')
+  })
+
+app.listen(PORT, () => {
+    console.log("==> Server is Connected")
+});
