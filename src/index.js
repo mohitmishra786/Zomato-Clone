17c745e578750316b99254edd91e0350ebe8df26
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-19 15:41:15 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const router = require("./routes");
+const mongoose = require('mongoose')
 
+mongoose
+  .connect('mongodb://localhost:27017/zomato_clone')
+  .then((res) => console.log("==> Connectd to DB"))
+  .catch((error) => console.log("==>Unable to connect DB"))
 const PORT = 2000;
 
 // app.get("/user/delete", (req, res) => {
---
+++ src/models/users/schema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const UsserSchema = new Schema({
+    name: {type: String , minlength:3, maxlength: 110},
+    phone: {type: Number, unique: true , minlength:10, maxlength: 13},
+    email: {type: String , unique: true, lowercase: true},
+    password: {type: String, minlength: 8, maxlength:16},
+    address: new Schema{
+        name: String,
+        address: String,
+        zip: Number,
+    },
+    role: {type: String},
+    orders: [String],
+    restaurant:  {type : String}
+    status: {type: Boolean},
+});
+module.export = mongoose.model('User' , UsserSchema);
