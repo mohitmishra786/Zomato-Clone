1592ce7e9822e7b4a0ce093bf425981b652cfb34
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-19 15:49:55 +0530

    update

--- src/models/users/schema.js
+++ src/models/users/schema.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const UsserSchema = new Schema({
-    name: {type: String , minlength:3, maxlength: 110},
-    phone: {type: Number, unique: true , minlength:10, maxlength: 13},
-    email: {type: String , unique: true, lowercase: true},
-    password: {type: String, minlength: 8, maxlength:16},
+    name: {type: String , required: true},
+    phone: {type: Number, unique: true , minlength:10, maxlength: 13,required: true},
+    email: {type: String , unique: true, lowercase: true,required: true},
+    password: {type: String, minlength: 8, maxlength:16,required: true},
     address: new Schema({
         name: String,
         address: String,
         zip: Number,
     }),
-    role: {type: String},
+    role: {type: String,required: true},
     orders: [String],
     restaurant:  {type : String},
-    status: {type: Boolean},
+    status: {type: Boolean,required: true},
 });
 module.exports = mongoose.model('User' , UsserSchema);
