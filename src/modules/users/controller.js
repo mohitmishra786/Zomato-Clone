418e160dde29ce77b823e4f5668092ded64a8973
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-19 16:01:17 +0530

    update

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -6,5 +6,15 @@
 const { create,deleteOne,get,update } = require('../../models/users/index');
 
 exports.create = (req,res) => {
-  const newUser = await User.create(req,body);
+  const newUser = await User.create(req,body).catch(error => {
+    return res.status(409).send({
+      success: false,
+      message : "unable to save user",
+    })
+  });
+  res.statu(200).send({
+    success:true,
+    data: newUser,
+    message: "Successfully saved the user",
+  })
 };
